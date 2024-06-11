87c8a35cb616974d920f8ef770fc9b2aba56bcc0
fjand5 <huynhtheluat@gmail.com>
2024-06-11 22:41:51 +0700

    bai 36

--- components/bluetooth/bluetooth.cpp
+++ components/bluetooth/bluetooth.cpp
@@ -9,7 +9,10 @@
 #include "nvs_flash.h"
 #include "esp_log.h"
 #include "algorithm"
+#include "esp_console.h"
+#include "argtable3/argtable3.h"
 
+auto typeMessage = xMessageBufferCreate(sizeof(esp_hidd_status_t) + sizeof(size_t));
 void startBluetooth(void)
 {
   if (nvs_flash_init() != ESP_OK)
@@ -59,22 +62,25 @@ void startBluetooth(void)
           new esp_hidd_qos_param_t{},
           new esp_hidd_qos_param_t{});
       break;
+    case ESP_HIDD_SEND_REPORT_EVT:
+      xMessageBufferSend(typeMessage, &(param->send_report.status), sizeof(esp_hidd_status_t), 0);
+      break;
     case ESP_HIDD_OPEN_EVT:
-      xTaskCreate(
-          [](void *p)
-          {
-            uint8_t data = 0;
-            while (1)
-            {
-              data = 0x05; //b
-              esp_bt_hid_device_send_report(ESP_HIDD_REPORT_TYPE_INTRDATA, 0, 1, &data);
-              data = 0x0; // nhả ra
-              vTaskDelay(pdMS_TO_TICKS(20));
-              esp_bt_hid_device_send_report(ESP_HIDD_REPORT_TYPE_INTRDATA, 0, 1, &data);
-              vTaskDelay(pdMS_TO_TICKS(1000));
-            }
-          },
-          "noname", 2000, nullptr, 1, nullptr);
+      // xTaskCreate(
+      //     [](void *p)
+      //     {
+      //       uint8_t data = 0;
+      //       while (1)
+      //       {
+      //         data = 0x05; //b
+      //         esp_bt_hid_device_send_report(ESP_HIDD_REPORT_TYPE_INTRDATA, 0, 1, &data);
+      //         data = 0x0; // nhả ra
+      //         vTaskDelay(pdMS_TO_TICKS(20));
+      //         esp_bt_hid_device_send_report(ESP_HIDD_REPORT_TYPE_INTRDATA, 0, 1, &data);
+      //         vTaskDelay(pdMS_TO_TICKS(1000));
+      //       }
+      //     },
+      //     "noname", 2000, nullptr, 1, nullptr);
       break;
 
     default:
@@ -112,4 +118,48 @@ void startBluetooth(void)
   //   esp_spp_enhanced_init(new esp_spp_cfg_t{ESP_SPP_MODE_CB, false, 0});
 
   esp_bt_gap_set_pin(ESP_BT_PIN_TYPE_FIXED, 4, new esp_bt_pin_code_t{'1', '2', '3', '4'});
+
+  esp_console_repl_config_t replConfig = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
+  esp_console_dev_uart_config_t uartConfig = ESP_CONSOLE_DEV_UART_CONFIG_DEFAULT();
+  esp_console_repl_t *repl = nullptr;
+  esp_console_new_repl_uart(&uartConfig, &replConfig, &repl);
+  esp_console_start_repl(repl);
+
+  esp_console_cmd_t typeCMD;
+  typeCMD.command = "type"; // type textksdjfbskdsdfsdftextksdjfbskdsdfsdftextksdjfbskdsdfsdftextksdjfbskdsdfsdftextksdjfbskdsdfsdftextksdjfbskdsdfsdftextksdjfbskdsdfsdf
+  typeCMD.func = [](int argc, char **argv)
+  {
+    auto type = [](uint8_t c)
+    {
+      esp_hidd_status_t status;
+      uint8_t data = c - 'a' + 0x04; // b
+    retry1:
+      esp_bt_hid_device_send_report(ESP_HIDD_REPORT_TYPE_INTRDATA, 0, 1, &data);
+      xMessageBufferReceive(typeMessage, &status, sizeof(esp_hidd_status_t), portMAX_DELAY);
+      if (status != ESP_HIDD_SUCCESS)
+        goto retry1;
+      data = 0x0; // nhả ra
+    retry2:
+      esp_bt_hid_device_send_report(ESP_HIDD_REPORT_TYPE_INTRDATA, 0, 1, &data);
+      xMessageBufferReceive(typeMessage, &status, sizeof(esp_hidd_status_t), portMAX_DELAY);
+      if (status != ESP_HIDD_SUCCESS)
+        goto retry2;
+    };
+     uint16_t i = 0;
+    while (1)
+    {
+      uint8_t c = argv[1][i];
+      if (c == 0)
+        break;
+      type(c);
+      i++;
+
+    }
+
+    return 0;
+  };
+  typeCMD.help = "go ban phim";
+  typeCMD.argtable = nullptr;
+
+  esp_console_cmd_register(&typeCMD);
 }
